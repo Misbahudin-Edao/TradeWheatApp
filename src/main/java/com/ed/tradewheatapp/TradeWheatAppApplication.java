72fc0c906c6d4214f67b66b2eaa84d9c834b792c
Misbahudin-Edao <misbahudinsirko@gmail.com>
2024-10-13 13:39:23 -0700

    Initial commit

---
+++ src/main/java/com/ed/tradewheatapp/TradeWheatAppApplication.java
@@ -0,0 +1,13 @@
+package com.ed.tradewheatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeWheatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeWheatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ed/tradewheatapp/TradeWheatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.tradewheatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeWheatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
